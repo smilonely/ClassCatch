11f7e617a3a84d3e676338ab43a953581b0f94a5
smilonely <34987095+smilonely@users.noreply.github.com>
2018-09-15 10:56:40 +0800

    0.1.1

--- classedit_xls.py
+++ classedit_xls.py
@@ -9,6 +9,7 @@ import re  #多字符分割字符相关
 import os
 import codecs
 
+DELIMITER_0 = "◇|\n"
 DELIMITER_1 = "◇"  #用于各个部分的分割
 DELIMITER_2 = "("  #分割周数与课程节次
 DELIMITER_3 = ","
@@ -41,6 +42,7 @@ def begin_time():
 			print ("\n输入有误，请重新输入：\n")
 			ver = 0
 	return date_time
+#获取时间并转化成datetime
 
 def time_verification(date_time, num):
 	while date_time - start_list[num] < datetime.timedelta(minutes = 1):
@@ -50,7 +52,7 @@ def time_verification(date_time, num):
 		date_time = datetime.datetime.strptime(time, "%Y-%m-%d-%H-%M")
 
 	return date_time
-
+#验证时间早晚
 
 start_list = [i for i in range(0, 13)]
 #课程开始时间list
@@ -62,22 +64,23 @@ print ("（无论第一节有没有课），\n")
 print ("格式为「年-月-日-时-分」（24小时制，不足两位请补0），\n")
 print ("例如：2001-01-01-09-59。\n")
 print ("请输入：\n")
-c01_sta = begin_time()
-start_list[1] = c01_sta
+#start_list[1] = begin_time()
+start_list[1] = datetime.datetime.strptime("2018-09-10-08-30", "%Y-%m-%d-%H-%M")
 
 print ("\n好的！下面再输入星期一下午第一节课的上课日期和时间，\n")
 print ("也就是第五节课，\n")
 print ("格式相同。\n")
-c05_sta = time_verification(begin_time(), 1)
-start_list[5] = c05_sta
+#start_list[5] = time_verification(begin_time(), 1)
+start_list[5] = datetime.datetime.strptime("2018-09-10-13-40", "%Y-%m-%d-%H-%M")
 
 print ("\n最后，再输入星期一晚上第一节课的上课日期和时间，\n")
 print ("我没记错的话，应该就是第九节课，\n")
 print ("格式依然相同。\n")
-c09_sta = time_verification(begin_time(), 5)
-start_list[9] = c09_sta
+#start_list[9] = time_verification(begin_time(), 5)
+start_list[9] = datetime.datetime.strptime("2018-09-10-18-20", "%Y-%m-%d-%H-%M")
 
 print("\n好的，请稍等……\n")
+
 
 def plus_60(time):
 	time_after = time + datetime.timedelta(minutes = 60)
@@ -99,13 +102,14 @@ def plus_week(time, week):
 	time_after = time + datetime.timedelta(weeks = week)
 	return time_after
 
+
 for i in range(0, 3):
 	start_list[2+4*i] = plus_50(start_list[1+4*i])
 	start_list[3+4*i] = plus_60(start_list[2+4*i])
 	start_list[4+4*i] = plus_50(start_list[3+4*i])
 	for l in range(1,5):
 		end_list[l+4*i] = plus_45(start_list[l+4*i])
-	#推算每节课的时间
+#推算每节课的时间
 
 
 def uid_born():
@@ -118,24 +122,31 @@ def uid_born():
 
 
 def classedit_xls(data_slice_1, y, x):		
+	global write_text
+	#引入全局变量 write_text
 
 	week_list = [0,"MO", "TU", "WE", "TH", "FR", "SA", "SU"]
 
+	print (list(data_slice_1))
+
 	name = data_slice_1[0]
-	week = week_list[x-1]
+	name = " ".join(name.split())  #去掉课程名字前的空格
+	week = week_list[x-1]		
+	ver_2 = 1
+	ver_3 = 1
+	try:
+		location = data_slice_1[2]
+	except:
+		ver_2 = 0
+	try:
+		description = "教师：" + data_slice_1[3]
+	except:
+		ver_3 = 0
+	#验证是否有地点和教师
 
 	data_slice_2 = data_slice_1[1].split(DELIMITER_2)
 		
 	loop = data_slice_1[1].count(DELIMITER_3)
-		
-	try:
-		location = data_slice_1[2]
-		description = "教师：" + data_slice_1[3]
-
-	except:
-		location = ""
-		description = ""
-
 	for i in range(0, loop):
 		data_slice_3 = re.split(DELIMITER_4, data_slice_2[0])
 			
@@ -148,26 +159,21 @@ def classedit_xls(data_slice_1, y, x):
 		start_lesson = start_lesson.strftime("%Y%m%dT%H%M%S")
 		end_lesson = end_lesson.strftime("%Y%m%dT%H%M%S")
 
-		write = []
-
-		write.append("BEGIN:VEVENT")
-		write.append("UID:" + uid_born() + "@github.com/smilonely")
-		write.append("DTSTART;TZID=Asia/Shanghai:" + start_lesson)
-		write.append("DTEND;TZID=Asia/Shanghai:0" + end_lesson)
-		write.append("RRULE:FREQ=WEEKLY;WKST=SU;COUNT=" + \
+		write_text.append("BEGIN:VEVENT")
+		write_text.append("UID:" + uid_born() + "@github.com/smilonely")
+		write_text.append("DTSTART;TZID=Asia/Shanghai:" + start_lesson)
+		write_text.append("DTEND;TZID=Asia/Shanghai:0" + end_lesson)
+		write_text.append("RRULE:FREQ=WEEKLY;WKST=SU;COUNT=" + \
 			str(duration_week) + ";BYDAY=" + week)
-		try:
-			write.append("DESCRIPTION:" + description)
-			write.append("LOCATION:" + location)
-		except:
-			print (2)
-		write.append("STATUS:CONFIRMED")
-		write.append("SUMMARY:" + name)
-		write.append("TRANSP:OPAQUE")
-		write.append("END:VEVENT\n")
-		
-	write_done = "\n".join(write)
-	return write_done
+		if ver_2 == 1:
+			write_text.append("LOCATION:" + location)
+		if ver_3 == 1:
+			write_text.append("DESCRIPTION:" + description)
+		write_text.append("STATUS:CONFIRMED")
+		write_text.append("SUMMARY:" + name)
+		write_text.append("TRANSP:OPAQUE")
+		write_text.append("END:VEVENT\n")
+#添加一个EVENT的内容进list write_text，但是不合并
 
 
 data_raw = xlrd.open_workbook("1.xls")
@@ -177,9 +183,9 @@ class_name = class_name.split(DELIMITER_5)
 class_name = class_name[0].split(DELIMITER_6)
 class_name = class_name[1]
 semester = table.cell(1, 7).value
+#找到班级和学期名字
 
-
-new_ics = codecs.open(class_name + "课程表.ics", "w", "utf-8")
+new_ics = codecs.open(class_name + "课程表.ics", "w","utf-8")
 new_ics.write(
 "BEGIN:VCALENDAR\n"
 "VERSION:2.0\n"
@@ -201,25 +207,40 @@ new_ics.write(
 "DTSTART:19700101T000000\n"
 "END:STANDARD\n"
 "END:VTIMEZONE)\n")
+#写入各项信息
 
 for y in range(3, 9):
 	for x in range(2, 9):
 		number = (table.cell(y, x).value).count(DELIMITER_1)
+		#数一数分隔符
 		data = table.cell(y, x).value
-		data_slice = data.split(DELIMITER_1)		
+		data_slice = re.split(DELIMITER_0, data)
+		write_text = []
 		
 		if number <= 3 and number > 0:
-			new_ics.write(classedit_xls(data_slice, y, x))
+			classedit_xls(data_slice, y, x)
+			write_done = "\n".join(write_text)
+			new_ics.write(write_done)
+			#直接将一个EVENT合并并写入
 
+		if number > 3:
+			j = number//3
 
-		elif number > 3:
-			l = number//3
-			for i in range(0, l):
-				data_slice_1 = data_slice[0+i*l:4+i*l]
-				new_ics.write(classedit_xls(data_slice_1, y, x))
+			for k in range(0, j):
+				data_slice_0 = data_slice[0+k*j:4+k*j]
+				classedit_xls(data_slice_0, y, x)
 
-		else:
+			write_done = "\n".join(write_text)
+			new_ics.write(write_done)
+			#将多个EVENT合并并写入
+			
+		write_text = ["\n"]
+		#初始化变量以重复写入
+
+		if number == 0:
 			break
+#三重循环，最为致命（雾）。三个循环来遍历单元格
+
 new_ics.write(u"END:VCALENDAR")
 new_ics.close()
 
